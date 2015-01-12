511655ee78744a125de8dd19c78fd4b04d55d041
lukemgriffith <lukemgriffith@gmail.com>
2015-01-12 22:46:24 +0000

    Added top 5 CPU to SQL DB
    For statement that adds the top 5 processes consuming the most CPU to a
    DB that holds PID, CPU, WorkingSet, and ProcessName

---
+++ top5CPUoutToSQL.ps1
@@ -0,0 +1,10 @@
+﻿$gpsOut = gps | sort cpu -Descending | select id, CPU, WorkingSet, ProcessName -First 5 
+
+
+$gpsOut | ForEach-Object {
+$SQLQuery = "use transactions
+go
+insert into processes
+Values ($($_.id),$($_.CPU),$($_.WorkingSet),'$($_.ProcessName)')
+"
+Invoke-Sqlcmd  $SQLQuery -Database transactions -ServerInstance SQL\SQLEXPRESS}
