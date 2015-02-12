bd1dc78d37a09ab6cb7e61bb9951895681bc8ee5
lukemgriffith <lukemgriffith@gmail.com>
2015-02-12 08:52:44 +0000

    NTP drift email monitor
    uses WMI and Chris Warwick Get-NtpTime.ps1 cmdlet to monitor drift
    between the server and NTP server. If above 30 seconds the script will
    send an email to outlook to wherever.

---
+++ NTP drift email monitor/NTPSync.ps1
@@ -0,0 +1,93 @@
+﻿<#
+Notes
+
+1. Server list needs to be obtained in string fomat.
+2. Secure string credentials need to be passed in.
+
+#>
+
+function Get-NTPDrift {
+
+# Obtaining Reed Servers
+$servers = <# 1. Obtain server list in string format #>
+
+#Import Get-NtpTime.ps1
+
+. 'Scr:\Client Specific\Reed\Get-NtpTime.ps1'
+
+
+# Variables requierd for StdRegProv
+$HKLM = 2147483650
+$key = "SYSTEM\CurrentControlSet\Services\W32Time\Parameters"
+$servervalue = "NtpServer"
+$typevalue = "Type"
+
+$finalOutput = @()
+
+foreach ($server in $servers) {
+
+    $testConnection = Test-Connection -ComputerName $server -Quiet -Count 1
+
+
+
+    if ($testConnection) {
+
+        $managedCred = <# 2. Get managed credentials #> 
+
+        # Declaring StdRegProv WMI classes 
+        $StdRegProvClass = Get-WmiObject -Class StdRegprov -Namespace root\default -List -ComputerName $server -Credential $managedCred
+
+        $timeSync = $StdRegProvClass | Invoke-WmiMethod -Name GetStringValue -ArgumentList $HKLM, $key, $servervalue
+
+        $timeSyncServerPrimary = ($timeSync.sValue -split " ")[0]
+        
+        $serverTimeRaw = Get-WmiObject -Class win32_operatingsystem -ComputerName $server -Credential $managedCred
+        $serverTime = $serverTimeRaw.ConvertToDateTime($serverTimeRaw.Localdatetime)
+        $serverTimeFormatted = get-date $serverTime
+
+        
+        try {
+        $timeServerTime =  Get-NtpTime -Server $timeSyncServerPrimary -NoDns | select -ExpandProperty ntpTime
+        }
+
+        catch [Microsoft.PowerShell.Commands.WriteErrorException] {
+        write-host "$timeSyncServerPrimary failed"
+        }
+
+        catch [System.Management.Automation.MethodInvocationException] {
+        write-host "$timeSyncServerPrimary failed"
+        }
+
+        $TimeServerTimeFormatted = get-date $timeServerTime
+
+        $timeSpan = New-TimeSpan -Start $serverTimeFormatted -End $TimeServerTimeFormatted | select Minutes, Seconds, Milliseconds
+
+    }
+
+        else {
+
+    write-host "$server was unable to connect"}
+
+
+    $output = New-Object -TypeName psobject
+    $output | Add-Member -MemberType NoteProperty -Name Server -Value $null
+    $output | Add-Member -MemberType NoteProperty -Name TimeServer -Value $null
+    $output | Add-Member -MemberType NoteProperty -Name MinuteDelay -Value $null
+    $output | Add-Member -MemberType NoteProperty -Name SecondDelay -Value $null
+    $output | Add-Member -MemberType NoteProperty -Name MillisecondDelay -Value $null
+
+    $output.Server = $server
+    $output.TimeServer = $timeSyncServerPrimary
+    $output.MinuteDelay = $timespan.Minutes
+    $output.SecondDelay = $timespan.Seconds
+    $output.MillisecondDelay = $timespan.Milliseconds
+
+    $finalOutput += $output
+
+
+}
+
+return $finalOutput
+
+}
+
---
+++ NTP drift email monitor/SendMail.ps1
@@ -0,0 +1,48 @@
+﻿<#
+Script checks for NTP drift over 30 seconds or 1 minute
+This is amended in the where-object cmdlet on $objects
+#>
+
+. 'Scr:\Client Specific\Reed\NTPSync.ps1'
+
+$object = Get-NTPDrift
+
+$objects = $object | ? {$_.SecondDelay -ge 30 -or $_.MinuteDelay -ge 1}
+
+if ($objects) {
+
+$Outlook = New-Object -ComObject Outlook.Application
+$Mail = $Outlook.CreateItem(0)
+$Mail.To = "*****"
+$Mail.Subject = "NTP Out Of Sync"
+$Mail.Body = @()
+
+$Mail.Body += 'the following servers are out of time with the NTP server'
+$Mail.Body += ''
+
+foreach ($item in $objects) {
+
+$Mail.Body += 'Server: ' + $item.Server
+$Mail.Body += 'TimeServer: ' + $item.TimeServer
+$Mail.Body += 'Minute Delay: ' + $item.MinuteDelay
+$Mail.Body += 'Second Delay: ' +  $item.SecondDelay
+$Mail.Body += 'MillisecondDelay: ' + $item.MillisecondDelay
+$Mail.Body += ''
+
+
+
+}
+
+
+$Mail.Send()
+
+Write-EventLog -LogName Application -Source "NTP Script" -EntryType Information -EventId 1 -Message "Email was sent"
+
+}
+
+else {
+
+Write-EventLog -LogName Application -Source "NTP Script" -EntryType Information -EventId 1 -Message "No issues were seen"
+
+} 
+
