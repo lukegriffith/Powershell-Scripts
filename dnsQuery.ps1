7782fc201a6e52ece1150778e2edd7289dfb5d9c
lukemgriffith <lukemgriffith@gmail.com>
2015-07-23 16:07:07 +0100

    dnsQuery
    dnsQuery

---
+++ dnsQuery.ps1
@@ -0,0 +1,7 @@
+﻿param ($dns)
+ 
+    while ($true) {
+    
+    [system.net.dns]::Resolve("$dns") | select @{name='HostName';expression={$_.hostname}}, @{name='AddressList';expression={$_.AddressList | foreach { "$_|"}}}, @{name='DateTime';expression={get-date}};
+    Start-Sleep -Seconds 30
+    }
