d87b22c651148cea78260669f761b5157bea6d85
Luke M Griffith <lukemgriffith@gmail.com>
2016-02-03 21:53:53 +0000

    Resolve-ArpTableReverseLookup
    function interrogates the arp table and attempts to perform a reverse
    lookup on all entries.

---
+++ Functions/Resolve-ArpTableReverseLookup.ps1
@@ -0,0 +1,22 @@
+﻿
+function Resolve-ArpTableReverseLookup {
+
+    $out = arp -a
+    $out = $out | select -Skip 3
+
+    $out | ForEach {
+        if ( $_ -match "\s+(?<IP>\S+)" ) { 
+            $res = [system.net.dns]::Resolve($matches.ip)
+
+            $res.AddressList | ForEach { 
+
+                [pscustomobject]@{
+                    HostName = $res.HostName
+                    Address = $_
+                }
+
+            }
+            
+        } 
+    }
+}
