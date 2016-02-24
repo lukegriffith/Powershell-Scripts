ff0cab2f09c04c334680e13ba03c6c6160a73085
Luke Griffith <lukemgriffith@gmail.com>
2016-02-24 21:45:55 +0000

    wmf5 dsc configuration

---
+++ DSC/wmf5dsc.ps1
@@ -0,0 +1,24 @@
+﻿set-location C:\temp
+
+Configuration WMF5 
+{
+
+    Import-DscResource -ModuleName xWindowsUpdate
+
+    Node 'srv' 
+    {
+
+        xHotfix KB3134759 
+        {
+
+            Id = 'KB3134759'
+            Ensure = 'Present'
+            Path = 'C:\temp\W2K12-KB3134759-x64.msu'
+        }
+    }
+
+}
+
+WMF5
+
+Start-DscConfiguration -Path .\WMF5 -Wait -Verbose -CimSession (New-CimSession -ComputerName 'srv') 
